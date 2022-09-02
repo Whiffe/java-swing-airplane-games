c7e08dd1e009b837e2373235b62adc4ee80a1055
Whiffe <33534050+Whiffe@users.noreply.github.com>
2022-09-02 09:42:30 +0800

    Update Main.java

--- src/com/game/view/Main.java
+++ src/com/game/view/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         new BaseFrame();
+        new MusicThread("src/video/bjmusic.wav").start();
 
     }
 
